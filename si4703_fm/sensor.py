9407170e7b9b731dcf697dc909e95f270e1dc11e
TiiMsvk <52893640+tiimsvk@users.noreply.github.com>
2025-11-08 19:09:14 +0100

    Update sensor.py

--- si4703_fm/sensor.py
+++ si4703_fm/sensor.py
@@ -1,36 +1,38 @@
-# custom_components/si4703_fm/sensor.py
-
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import sensor
-# Pouzivame iba zakladne konstanty. Jednotku ('dBµV') nastavujeme priamo ako retazec.
-from esphome.const import CONF_ID, CONF_NAME, CONF_ACCURACY_DECIMALS, ICON_SIGNAL
-
+from esphome.const import (
+    CONF_ID, 
+    CONF_ACCURACY_DECIMALS, 
+    ICON_SIGNAL,
+    #CONF_UPDATE_INTERVAL
+)
 # Import z __init__.py
 from . import si4703_fm_ns, Si4703FM, CONF_SI4703_FM_ID
 
-DEPENDENCIES = ['si4703_fm']
+# DEPENDENCIES = ['si4703_fm'] # Odstránené - spôsobuje circular dependency
 
 # Definícia schémy pre senzor RSSI
 CONFIG_SCHEMA = sensor.sensor_schema(
-    # Kritická oprava: Použitie reťazca pre jednotku obchádza chýbajúci import.
     unit_of_measurement='dBµV', 
     icon=ICON_SIGNAL,
     accuracy_decimals=0,
 ).extend({
     cv.GenerateID(): cv.declare_id(sensor.Sensor),
-    # Odkaz na hlavný hub (Si4703FM)
-    cv.Required(CONF_SI4703_FM_ID): cv.use_id(Si4703FM), 
-})
+
+    cv.Required(CONF_SI4703_FM_ID): cv.use_id(Si4703FM),
+    
+    # Senzor bude používať update_interval z hlavného hubu
+    # cv.Optional(CONF_UPDATE_INTERVAL, default='5s'): cv.positive_time_period_milliseconds,
+}).extend(cv.COMPONENT_SCHEMA) # PollingComponent tu nie je potrebný, Hub robí polling
 
 # Funkcia pre registráciu senzora
-def to_code(config):
-    # Nájdeme parent hub (Si4703FM)
-    hub = yield cg.get_variable(config[CONF_SI4703_FM_ID])
+async def to_code(config):
+    # 1. Získame hub
+    hub_var = await cg.get_variable(config[CONF_SI4703_FM_ID])
     
-    # Vytvoríme senzor
-    sens = cg.new_Pvariable(config[CONF_ID])
-    yield sensor.register_sensor(sens, config)
+    # 2. Vytvoríme štandardný senzor (toto nahradí new_Pvariable aj register_sensor)
+    var = await sensor.new_sensor(config)
     
-    # Priradíme senzor k hubu pomocou setter metódy
-    cg.add(hub.set_rssi_sensor(sens))
+    # 3. Pripojíme senzor k hubu
+    cg.add(hub_var.set_rssi_sensor(var))

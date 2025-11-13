2731ee376cd838217af834d2df077ff559e5f59e
TiiMsvk <52893640+tiimsvk@users.noreply.github.com>
2025-11-13 08:30:10 +0100

    Refactor si4703_fm.h to add new components

--- si4703_fm/si4703_fm.h
+++ si4703_fm/si4703_fm.h
@@ -7,7 +7,9 @@
 #include "esphome/components/switch/switch.h"
 #include "esphome/components/sensor/sensor.h" 
 #include "esphome/components/text_sensor/text_sensor.h"
-#include <string> // Pridané pre std::string a RDS
+#include "esphome/components/binary_sensor/binary_sensor.h"
+#include "esphome/components/button/button.h"
+#include <string>
 
 namespace esphome {
 namespace si4703_fm {
@@ -34,14 +36,17 @@ static const uint8_t READCHANNEL = 0x0B;
 // Bity, ktoré by mali byť v STATUSRSSI (0x0A)
 static const uint16_t RDSR = (1 << 15);  // Bit 15 - RDS Ready
 static const uint16_t RDSS = (1 << 11);  // Bit 11 - RDS Synchronized (Kľúčová oprava) 
+static const uint16_t STC = (1 << 14); // Seek/Tune Complete
+static const uint16_t SFBL = (1 << 13); // Seek/Tune Fail/Band Limit
 
 // Ostatné bity z iných registrov:
 static const uint16_t RDY = (1 << 15); // Ready bit v STATUSRSSI
 static const uint16_t RDS = (1 << 12);    // Bit v SYSCONFIG1 (0x04) na zapnutie RDS
+static const uint16_t DMUTE = (1 << 14);  // Bit v POWERCFG (0x02) - Mute Disable
 
 // Bity
 static const uint16_t TUNE = (1 << 15);
-static const uint16_t STC = (1 << 14); // Seek/Tune Complete
+//static const uint16_t STC = (1 << 14); // Seek/Tune Complete
 
 class Si4703FM; // Predbežná deklarácia
 
@@ -62,16 +67,6 @@ protected:
 class Si4703MuteSwitch : public switch_::Switch {
 public:
     Si4703MuteSwitch(Si4703FM *parent) : parent_(parent) {}
-    // Plná deklarácia write_state
-    void write_state(bool state) override; 
-
-protected:
-    Si4703FM *parent_;
-};
-
-class Si4703StereoMonoSwitch : public switch_::Switch {
-public:
-    Si4703StereoMonoSwitch(Si4703FM *parent) : parent_(parent) {}
     // Plná deklarácia write_state
     void write_state(bool state) override; 
 
@@ -103,6 +98,50 @@ protected:
     Si4703FM *parent_;
 };
 
+// =============================================================================
+// Binary Sensor triedy
+// =============================================================================
+class Si4703StereoIndicator : public binary_sensor::BinarySensor, public Component {
+ public:
+  Si4703StereoIndicator(Si4703FM *parent) : parent_(parent) {}
+ protected:
+  Si4703FM *parent_{nullptr};
+};
+
+// Trieda pre TP Indicator (Traffic Programme)
+class Si4703TPIndicator : public binary_sensor::BinarySensor, public Component {
+ public:
+  Si4703TPIndicator(Si4703FM *parent) : parent_(parent) {}
+ protected:
+  Si4703FM *parent_{nullptr};
+};
+
+// Trieda pre TA Indicator (Traffic Announcement)
+class Si4703TAIndicator : public binary_sensor::BinarySensor, public Component {
+ public:
+  Si4703TAIndicator(Si4703FM *parent) : parent_(parent) {}
+ protected:
+  Si4703FM *parent_{nullptr};
+};
+
+// =============================================================================
+// Button (Tlačidlo) triedy
+// =============================================================================
+class Si4703SeekUpButton : public button::Button, public Component {
+ public:
+  Si4703SeekUpButton(Si4703FM *parent) : parent_(parent) {}
+  void press_action() override; // Implementované v .cpp
+ protected:
+  Si4703FM *parent_{nullptr};
+};
+
+class Si4703SeekDownButton : public button::Button, public Component {
+ public:
+  Si4703SeekDownButton(Si4703FM *parent) : parent_(parent) {}
+  void press_action() override; // Implementované v .cpp
+ protected:
+  Si4703FM *parent_{nullptr};
+};
 
 // -----------------------------------------------------------------------------
 // Hlavná trieda (Hub)
@@ -119,15 +158,23 @@ public:
   // Settery pre prepojenie entít
   void set_frequency_number(Si4703Frequency *freq_num) { this->frequency_number_ = freq_num; }
   void set_rssi_sensor(sensor::Sensor *rssi_sensor) { this->rssi_sensor_ = rssi_sensor; }
-  void set_snr_sensor(sensor::Sensor *snr_sensor) { this->snr_sensor_ = snr_sensor; } // NOVÉ
-  void set_bler_a_sensor(sensor::Sensor *bler_a_sensor) { this->bler_a_sensor_ = bler_a_sensor; } // NOVÉ
-  void set_bler_d_sensor(sensor::Sensor *bler_d_sensor) { this->bler_d_sensor_ = bler_d_sensor; } // NOVÉ
+  void set_snr_sensor(sensor::Sensor *snr_sensor) { this->snr_sensor_ = snr_sensor; } //dev sensor
+  //void set_bler_a_sensor(sensor::Sensor *bler_a_sensor) { this->bler_a_sensor_ = bler_a_sensor; }   //dev sensor 
+  //void set_bler_d_sensor(sensor::Sensor *bler_d_sensor) { this->bler_d_sensor_ = bler_d_sensor; }   //dev sensor 
+  void set_pty_sensor(sensor::Sensor *pty_sensor) { this->pty_sensor_ = pty_sensor; }
   void set_volume_number(Si4703Volume *vol_num) { this->volume_number_ = vol_num; }
   void set_power_switch(Si4703PowerSwitch *power_sw) { this->power_switch_ = power_sw; }
   void set_mute_switch(Si4703MuteSwitch *mute_sw) { this->mute_switch_ = mute_sw; } 
-  void set_stereo_mono_switch(Si4703StereoMonoSwitch *st_mo_sw) { this->stereo_mono_switch_ = st_mo_sw; } 
   void set_rds_text_sensor(text_sensor::TextSensor *rds_text_sensor) { this->rds_text_sensor_ = rds_text_sensor; } 
   void set_rds_ps_sensor(text_sensor::TextSensor *rds_ps_sensor) { this->rds_ps_sensor_ = rds_ps_sensor; }
+  void set_rds_ct_sensor(text_sensor::TextSensor *rds_ct_sensor) { this->rds_ct_sensor_ = rds_ct_sensor; }
+  void set_stereo_indicator_sensor(binary_sensor::BinarySensor *s) { this->stereo_indicator_sensor_ = s; }
+  void set_tp_indicator_sensor(binary_sensor::BinarySensor *s) { this->tp_indicator_sensor_ = s; }
+  void set_ta_indicator_sensor(binary_sensor::BinarySensor *s) { this->ta_indicator_sensor_ = s; }
+  void set_seek_up_button(button::Button *btn) { this->seek_up_button_ = btn; }
+  void set_seek_down_button(button::Button *btn) { this->seek_down_button_ = btn; }
+  
+  void publish_stereo_status();
 
   // Interval aktualizácie
   void set_update_interval(uint32_t update_interval) { this->update_interval_ = update_interval; }
@@ -136,16 +183,20 @@ public:
   void set_channel_from_float(float freq_mhz);
   float get_current_frequency_mhz();
   float get_rssi();
-  float get_snr(); // NOVÉ
-  float get_bler_a(); // NOVÉ
-  float get_bler_d(); // NOVÉ
+  float get_snr(); // dev sensor
+  //float get_bler_a(); // dev sensor
+  //float get_bler_d(); // dev sensor
+  float get_pty_code();
   bool set_volume(uint8_t volume); 
   uint8_t get_volume(); 
+  void publish_tp_ta_status();
   
   bool turn_on();
   bool turn_off();
   bool mute();   
-  bool unmute(); 
+  bool unmute(bool restore_volume = true);
+  void seek_up();
+  void seek_down();
   
   bool set_mono_mode(bool mono_on); 
   bool is_mono_mode();             
@@ -160,6 +211,7 @@ protected:
   GPIOPin *stc_int_pin_;
 
   uint16_t registers_[16] = {0};
+  int volume_ = 8;
   uint8_t previous_volume_ = 8;
 
   // --- Premenné pre RDS Program Service (PS) ---
@@ -173,28 +225,47 @@ protected:
   uint32_t last_rds_check_ = 0;
   static const uint32_t RDS_CHECK_INTERVAL = 50;
   
+  // --- NOVÉ: Premenné pre RDS Clock Time (CT) ---
+  char rds_ct_buffer_[20]; // Buffer pre formátovaný čas, napr. "HH:MM"
+  
+    // --- NOVÉ: Premenné pre stabilizáciu PTY ---
+  uint8_t last_pty_code_ = 255; // Posledný prijatý PTY kód pre overenie
+  uint8_t pty_validation_count_ = 0; // Počet po sebe idúcich rovnakých PTY kódov
+  static const uint8_t PTY_VALIDATION_THRESHOLD = 2; // Koľkokrát musí byť PTY rovnaký, než sa publikuje
+  
   // Pointers na entity
   Si4703Frequency *frequency_number_ = nullptr;
   sensor::Sensor *rssi_sensor_{nullptr};
-  sensor::Sensor *snr_sensor_{nullptr}; // NOVÉ: SNR
-  sensor::Sensor *bler_a_sensor_{nullptr}; // NOVÉ: BLER A
-  sensor::Sensor *bler_d_sensor_{nullptr}; // NOVÉ: BLER D
+  sensor::Sensor *snr_sensor_{nullptr}; // SNR dev sensor
+  //sensor::Sensor *bler_a_sensor_{nullptr}; // BLER A dev sensor
+  //sensor::Sensor *bler_d_sensor_{nullptr}; // BLER D dev sensor
+  sensor::Sensor *pty_sensor_{nullptr};
   Si4703Volume *volume_number_ = nullptr; 
   Si4703PowerSwitch *power_switch_ = nullptr;
   Si4703MuteSwitch *mute_switch_ = nullptr; 
-  Si4703StereoMonoSwitch *stereo_mono_switch_ = nullptr; 
+  binary_sensor::BinarySensor *stereo_indicator_sensor_{nullptr};
+  binary_sensor::BinarySensor *tp_indicator_sensor_{nullptr};
+  binary_sensor::BinarySensor *ta_indicator_sensor_{nullptr};
   text_sensor::TextSensor *rds_text_sensor_{nullptr};
   text_sensor::TextSensor *rds_ps_sensor_{nullptr};
+  text_sensor::TextSensor *rds_ct_sensor_{nullptr};
+  button::Button *seek_up_button_{nullptr};
+  button::Button *seek_down_button_{nullptr};
   
   uint32_t update_interval_ = 50; 
   uint32_t last_update_ = 0;
+  uint32_t last_stereo_update_ = 0;
+  static const uint32_t STEREO_UPDATE_INTERVAL = 300;
 
   bool tune_to_channel(uint16_t channel_val);
   uint16_t get_channel_value();
   void process_rds_data(); 
-  
+    
   void process_rds();
   void decode_rds_data(uint16_t block_a, uint16_t block_b, uint16_t block_c, uint16_t block_d);
+  
+  void seek_internal(bool seek_up);
+  void reset_rds_on_tune();
   
 };
 

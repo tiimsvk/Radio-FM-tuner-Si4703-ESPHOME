5027c1083772d685668fc37bd04f9d92c231f222
TiiMsvk <52893640+tiimsvk@users.noreply.github.com>
2025-11-13 08:30:22 +0100

    Update switch.py

--- si4703_fm/switch.py
+++ si4703_fm/switch.py
@@ -6,14 +6,13 @@ from esphome.components import switch
 from esphome.const import CONF_ID, CONF_NAME, CONF_ICON, ENTITY_CATEGORY_CONFIG 
 
 # Importujeme hlavné triedy z __init__.py
-from . import Si4703FM, Si4703PowerSwitch, CONF_SI4703_FM_ID
+from . import Si4703FM, Si4703PowerSwitch, Si4703MuteSwitch, CONF_SI4703_FM_ID
 
-# Kľúč pre YAML
+# Kľúče pre YAML
 CONF_POWER_SWITCH = "power_switch"
-
+CONF_MUTE_SWITCH = "mute_switch" # <--- NOVÉ
 
 # --- Schéma pre Power Switch (LEN pre entitu, bez ID hubu) ---
-# Používame switch.switch_schema, ktorá sa stará o register komponentu a entitu
 POWER_SCHEMA = switch.switch_schema(
     Si4703PowerSwitch,
     entity_category=ENTITY_CATEGORY_CONFIG, # Nastaví kategóriu na Konfiguračnú
@@ -25,6 +24,16 @@ POWER_SCHEMA = switch.switch_schema(
     }
 ).extend(cv.COMPONENT_SCHEMA)
 
+# --- NOVÉ: Schéma pre Mute Switch ---
+MUTE_SCHEMA = switch.switch_schema(
+    Si4703MuteSwitch,
+    entity_category=ENTITY_CATEGORY_CONFIG, # Nastaví kategóriu na Konfiguračnú
+).extend(
+    {
+        cv.Optional(CONF_NAME, default="Rádio Mute"): cv.string,
+        cv.Optional(CONF_ICON, default="mdi:volume-off"): cv.icon,
+    }
+).extend(cv.COMPONENT_SCHEMA)
 
 # --- Hlavná schéma pre platformu 'switch.si4703_fm' (S ID hubu na hornej úrovni) ---
 CONFIG_SCHEMA = cv.Schema(
@@ -34,23 +43,23 @@ CONFIG_SCHEMA = cv.Schema(
         
         # Power switch je teraz definovaný ako list alebo len jedna entita
         cv.Required(CONF_POWER_SWITCH): POWER_SCHEMA,
+        cv.Optional(CONF_MUTE_SWITCH): MUTE_SCHEMA,
     }
 ).extend(cv.COMPONENT_SCHEMA) # Predĺžime o základnú komponentnú schému
 
 
 async def to_code(config):
-    # 1. Získame odkaz na hlavný hub (ID hubu je na hornej úrovni 'config')
     hub = await cg.get_variable(config[CONF_SI4703_FM_ID])
 
-    # 2. Vytvoríme switch
+    # Kód pre Power Switch
     conf = config[CONF_POWER_SWITCH]
-    
-    # Vytvorenie inštancie triedy Si4703PowerSwitch (s hubom ako parent)
-    var = cg.new_Pvariable(conf[CONF_ID], hub) 
-    
-    # 3. Zaregistrujeme ho
-    await cg.register_component(var, conf)
-    await switch.register_switch(var, conf) # Správna registrácia switch entít
-    
-    # 4. Pripojíme pointer na switch k hlavnému hubu (cez setter)
+    var = cg.new_Pvariable(conf[CONF_ID], hub)
+    await switch.register_switch(var, conf) 
     cg.add(hub.set_power_switch(var))
+
+    # Kód pre Mute Switch
+    if CONF_MUTE_SWITCH in config:
+        conf = config[CONF_MUTE_SWITCH]
+        var = cg.new_Pvariable(conf[CONF_ID], hub)
+        await switch.register_switch(var, conf)
+        cg.add(hub.set_mute_switch(var))

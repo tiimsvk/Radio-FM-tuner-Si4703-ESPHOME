fea1d3ebedf66ccf55d561f289acb833537db997
TiiMsvk <52893640+tiimsvk@users.noreply.github.com>
2025-11-13 08:30:36 +0100

    Update text_sensor.py

--- si4703_fm/text_sensor.py
+++ si4703_fm/text_sensor.py
@@ -3,57 +3,49 @@ import esphome.config_validation as cv
 from esphome.components import text_sensor
 from esphome.const import CONF_ID, CONF_NAME, CONF_ICON
 
-# Import z __init__.py
-from . import si4703_fm_ns, Si4703FM, CONF_SI4703_FM_ID
+from . import Si4703FM, CONF_SI4703_FM_ID
 
-# ODSTRÁNENÁ RIADKA: Si4703RdsTextSensor = si4703_fm_ns.class_("Si4703RdsTextSensor", cg.Component, text_sensor.TextSensor)
-# Teraz priamo používame text_sensor.TextSensor ako typ!
-
-# Definujeme kľúč
 CONF_RDS_TEXT = "rds_text"
 CONF_RDS_PS = "rds_ps"
+CONF_RDS_CT = "rds_ct"
 
-# --- Schéma pre RDS PS Sensor ---
-RDS_PS_SCHEMA = text_sensor.TEXT_SENSOR_SCHEMA.extend(
-    {
-        cv.GenerateID(): cv.declare_id(text_sensor.TextSensor), 
-        cv.Optional(CONF_NAME, default="FM Názov Stanice"): cv.string, # Zmena default názvu
-        cv.Optional(text_sensor.CONF_ICON, default="mdi:radio"): cv.icon, # Iná ikona
-    }
-).extend(cv.COMPONENT_SCHEMA)
+# --- Schémy ---
+RDS_PS_SCHEMA = text_sensor.text_sensor_schema(
+    icon="mdi:radio",
+)
 
-# --- Schéma pre RDS Text Sensor ---
-RDS_TEXT_SCHEMA = text_sensor.TEXT_SENSOR_SCHEMA.extend(
-    {
-        # Používame štandardné ID pre text_sensor.TextSensor
-        cv.GenerateID(): cv.declare_id(text_sensor.TextSensor), 
-        cv.Optional(CONF_NAME, default="FM Rádio Text"): cv.string,
-        cv.Optional(text_sensor.CONF_ICON, default=CONF_ICON): cv.icon,
-    }
-).extend(cv.COMPONENT_SCHEMA)
+RDS_TEXT_SCHEMA = text_sensor.text_sensor_schema(
+    icon="mdi:home-assistant",
+)
 
-# --- Hlavná schéma pre platformu 'text_sensor.si4703_fm' ---
+RDS_CT_SCHEMA = text_sensor.text_sensor_schema(
+    icon="mdi:clock-check-outline",
+)
+
 CONFIG_SCHEMA = cv.Schema(
     {
         cv.Required(CONF_SI4703_FM_ID): cv.use_id(Si4703FM),
         cv.Optional(CONF_RDS_TEXT): RDS_TEXT_SCHEMA,
         cv.Optional(CONF_RDS_PS): RDS_PS_SCHEMA,
+        cv.Optional(CONF_RDS_CT): RDS_CT_SCHEMA,
     }
 ).extend(cv.COMPONENT_SCHEMA)
 
 
 async def to_code(config):
-    # KĽÚČOVÁ OPRAVA: Premenná hub_var musí byť definovaná prvá a UNIVERZÁLNE!
     hub_var = await cg.get_variable(config[CONF_SI4703_FM_ID])
 
-    # Generovanie RT senzora (pôvodný kód)
     if CONF_RDS_TEXT in config:
-        text_sensor_var = await text_sensor.new_text_sensor(config[CONF_RDS_TEXT])
-        # Riadok 50: Teraz je hub_var garantovane definovaný.
+        text_conf = config[CONF_RDS_TEXT]
+        text_sensor_var = await text_sensor.new_text_sensor(text_conf)
         cg.add(hub_var.set_rds_text_sensor(text_sensor_var))
 
-    # Generovanie PS senzora (NOVÝ kód)
     if CONF_RDS_PS in config:
-        ps_sensor_var = await text_sensor.new_text_sensor(config[CONF_RDS_PS])
-        # Použitie už definovanej hub_var
+        ps_conf = config[CONF_RDS_PS]
+        ps_sensor_var = await text_sensor.new_text_sensor(ps_conf)
         cg.add(hub_var.set_rds_ps_sensor(ps_sensor_var))
+
+    if CONF_RDS_CT in config:
+        ct_conf = config[CONF_RDS_CT]
+        ct_sensor_var = await text_sensor.new_text_sensor(ct_conf)
+        cg.add(hub_var.set_rds_ct_sensor(ct_sensor_var))

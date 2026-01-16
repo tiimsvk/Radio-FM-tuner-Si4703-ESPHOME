400a403d023e166cfc744bdcc55f935adb713df6
TiiMsvk <52893640+tiimsvk@users.noreply.github.com>
2026-01-16 10:45:35 +0100

    Update __init__.py

--- si4703_fm/__init__.py
+++ si4703_fm/__init__.py
@@ -2,11 +2,7 @@ import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome import pins
 from esphome.components import i2c, number, sensor, switch, text_sensor, binary_sensor, button
-from esphome.const import (
-    CONF_ID, 
-    CONF_ADDRESS, 
-    CONF_UPDATE_INTERVAL
-)
+from esphome.const import CONF_ID, CONF_ADDRESS, CONF_UPDATE_INTERVAL
 
 DEPENDENCIES = ["i2c"]
 # Povieme ESPHome, aby hľadalo number.py a sensor.py
@@ -20,9 +16,12 @@ Si4703Frequency = si4703_fm_ns.class_("Si4703Frequency", cg.Component, number.Nu
 Si4703Volume = si4703_fm_ns.class_("Si4703Volume", cg.Component, number.Number)
 Si4703PowerSwitch = si4703_fm_ns.class_("Si4703PowerSwitch", cg.Component, switch.Switch)
 Si4703MuteSwitch = si4703_fm_ns.class_("Si4703MuteSwitch", cg.Component, switch.Switch)
+Si4703AmpSwitch = si4703_fm_ns.class_("Si4703AmpSwitch", cg.Component, switch.Switch)
+Si4703Gpio2Switch = si4703_fm_ns.class_("Si4703Gpio2Switch", cg.Component, switch.Switch)
 Si4703StereoIndicator = si4703_fm_ns.class_("Si4703StereoIndicator", cg.Component, binary_sensor.BinarySensor)
 Si4703TPIndicator = si4703_fm_ns.class_("Si4703TPIndicator", cg.Component, binary_sensor.BinarySensor)
 Si4703TAIndicator = si4703_fm_ns.class_("Si4703TAIndicator", cg.Component, binary_sensor.BinarySensor)
+Si4703Gpio2Sensor = si4703_fm_ns.class_("Si4703Gpio2Sensor", cg.Component, binary_sensor.BinarySensor)
 Si4703SeekUpButton = si4703_fm_ns.class_("Si4703SeekUpButton", cg.Component, button.Button)
 Si4703SeekDownButton = si4703_fm_ns.class_("Si4703SeekDownButton", cg.Component, button.Button)
 
@@ -30,7 +29,6 @@ Si4703SeekDownButton = si4703_fm_ns.class_("Si4703SeekDownButton", cg.Component,
 CONF_SI4703_FM_ID = "si4703_fm_id"
 CONF_RESET_PIN = "reset_pin"
 CONF_STC_INT_PIN = "stc_int_pin"
-CONF_SI4703_FM_ID = "si4703_fm_id"
 
 # Toto je schéma pre hlavný blok si4703_fm:
 CONFIG_SCHEMA = cv.Schema(
@@ -39,8 +37,7 @@ CONFIG_SCHEMA = cv.Schema(
         cv.Required(CONF_RESET_PIN): pins.gpio_output_pin_schema,
         # STC pin je teraz voliteľný, aby sa ušetrilo GPIO
         cv.Optional(CONF_STC_INT_PIN): pins.gpio_input_pin_schema,
-        # Interval aktualizácie pre RSSI a stavy
-        cv.Optional(CONF_UPDATE_INTERVAL, default='2s'): cv.positive_time_period_milliseconds,
+        cv.Optional(CONF_UPDATE_INTERVAL, default="2s"): cv.positive_time_period_milliseconds,
     }
 ).extend(i2c.i2c_device_schema(0x10)).extend(cv.COMPONENT_SCHEMA)
 
@@ -48,7 +45,6 @@ CONFIG_SCHEMA = cv.Schema(
 async def to_code(config):
     # Tento kód inicializuje hlavný komponent Si4703FM
     parent = await cg.get_variable(config[i2c.CONF_I2C_ID])
-    
     reset_pin = await cg.gpio_pin_expression(config[CONF_RESET_PIN])
     
     stc_pin = None
@@ -59,7 +55,7 @@ async def to_code(config):
         config[CONF_ID], 
         parent, 
         reset_pin,
-        stc_pin
+        stc_pin,
     )
     await cg.register_component(var, config)
     await i2c.register_i2c_device(var, config)
